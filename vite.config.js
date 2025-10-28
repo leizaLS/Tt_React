7628c62f19174816701ead8c8c8de076af98e1e1
LeizaLS <leandrosolano20@gmail.com>
2025-10-27 21:31:45 -0300

    footer

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import react from '@vitejs/plugin-react'
 //Proxy steam
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Raiz proyecto
+  base: '/Tt_React/', // Raiz proyecto
   server: {
     proxy: {
       '/steam-api': {
